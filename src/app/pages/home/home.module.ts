5f649b9e81dafa01b4214d64aa543715aa50c278
James <james.bidule@gmail.com>
2023-06-21 18:34:23 +0200

    fix(module): Add routing to home and not-found modules
    The changes in this commit add routing to the home and not-found modules. The RouterModule was imported into both modules, and the NotFoundComponent was declared in the not-found module. Additionally, AppRoutingModule was imported into the not-found module.

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ HomeComponent ],
   imports: [
     CommonModule,
+    RouterModule,
     SharedModule,
     SlickCarouselModule
   ]
--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { NotFoundComponent } from './not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class NotFoundModule { }
