e244b0451423990d6aea66bbf47cb537f9a6a31c
James ABIB <james.bidule@gmail.com>
2023-06-23 22:52:17 +0200

    fix(auth): Fix header component logout functionality
    The header component now displays the logout button when a user is logged in and removes it when they log out. The profile logo now links to the correct page based on whether or not a user is logged in. Additionally, an admin button is displayed for users with admin privileges.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit  } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  link:string = "auth";
+ 
   constructor(private Router: Router) { }
 
   ngOnInit(): void {
 
+    if(localStorage.getItem('token')) {
+      document.getElementById('logout')?.classList.remove('invisible');
+      this.link = "profile";
+    }
+
+    if(localStorage.getItem('token') && localStorage.getItem('role') === 'admin') {
+      document.getElementById('admin')?.classList.remove('d-none');
+    }
+
+  }
+
+  logout() {  
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    this.Router.navigate(['/auth']);
   }
 }
