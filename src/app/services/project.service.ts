31e7fd6c39efb30da0945a287073c1c0707174af
James ABIB <james.bidule@gmail.com>
2023-06-25 17:19:18 +0200

    fix(project): Add authorization header to project service
    The project service now includes an authorization header in its HTTP requests. The CommonService is used to retrieve the token from local storage and set it as a bearer token in the headers. This ensures that only authorized users can access project data.

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,24 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Project } from '../models/project.model';
-import { catchError, lastValueFrom, throwError } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
+import { CommonService } from './common.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
   private apiUrl = `${environment.apiUrl}/project`;
+  token: string | null = null;
+  headers: HttpHeaders | null = null;
+  options: {headers: HttpHeaders};
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private commonService: CommonService) { 
+    this.token = this.commonService.getLocalStorageItem('token');
+    this.headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
+    this.options = { headers: this.headers };
+  }
 
   async getProject(id: string | null): Promise<Project> {
     if (!id) {
       throw new Error('Project not found');
     }
 
-    const project = await lastValueFrom(this.http.get<Project>(`${this.apiUrl}/${id}`));
-    
+    const project = await lastValueFrom(this.http.get<Project>(`${this.apiUrl}/${id}`, this.options));
     if (!project) {
       throw new Error('Project not found');
     }
