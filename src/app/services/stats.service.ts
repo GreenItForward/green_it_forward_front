3475da4e0bd4d82516f9828d81751d4c11c944f5
James <james.bidule@gmail.com>
2023-06-27 18:27:48 +0200

    feat(stats): implement endpoints stats api
    This commit adds a new service called StatsService that provides methods to retrieve statistics related to users and projects. The service uses HttpClient to make API calls and implements async/await for better performance. It also includes a constructor that initializes the necessary headers for authorization.

---
+++ src/app/services/stats.service.ts
@@ -0,0 +1,33 @@
+import { lastValueFrom } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CommonService } from './common.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatsService {
+  private apiUrl = `${environment.apiUrl}/stats`;
+  token: string | null = null;
+  headers: HttpHeaders | null = null;
+  options: {headers: HttpHeaders};
+  
+  constructor(private httpClient:HttpClient, private commonService: CommonService) {
+    this.token = this.commonService.getLocalStorageItem('token');
+    this.headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
+    this.options = { headers: this.headers };
+   }
+
+    async getTotalUsers(): Promise<number> {
+      return await lastValueFrom(this.httpClient.get<number>(`${this.apiUrl}/total-users`, this.options));
+    }
+
+    async getTotalProjects(): Promise<number> {
+      return await lastValueFrom(this.httpClient.get<number>(`${this.apiUrl}/total-projects`, this.options));
+    }
+
+    async getUsersPerMonth(year: number): Promise<{[key: string]: number}> {
+      return await lastValueFrom(this.httpClient.get<{[key: string]: number}>(`${this.apiUrl}/users-per-month/${year}`, this.options));
+    }
+}
