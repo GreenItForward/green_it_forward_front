e324bbc85b55dffaa631a33044e2a69ab4cf9962
James ABIB <james.bidule@gmail.com>
2023-07-11 22:27:25 +0200

    fix(project): resolving load image

--- src/app/pages/projects/project/project.component.ts
+++ src/app/pages/projects/project/project.component.ts
@@ -4,6 +4,7 @@ import { Project } from 'src/app/models/project.model';
 import { Location } from '@angular/common';
 import { CommonService } from 'src/app/services/common.service';
 import { DateService } from 'src/app/services/date.service';
+import { UploadService } from 'src/app/services/upload.service';
 
 @Component({
   selector: 'app-project',
@@ -14,14 +15,21 @@ export class ProjectComponent {
   project:Project;
   creationDate:string;
   endDate:string;
+  imageFile: File;
+  imageSrc: string;
+  noImage: boolean = true;
 
 
   constructor(private activatedRoute: ActivatedRoute, private location: Location, protected commonService: CommonService, 
-    protected dateService: DateService) { }
+    protected dateService: DateService, private uploadService: UploadService) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     const data = this.activatedRoute.snapshot.data as RouteData;
     this.project = data.project;
+    await this.loadImage();
+    if (!this.project.imageUrl) {
+      this.project.imageUrl = 'assets/background.jpeg';
+    }
 
     this.creationDate = this.dateService.formatRelativeTime(this.project.startDate, "Débuté depuis");
     this.endDate = this.dateService.formatRelativeTime(this.project.endDate, "Se termine dans");
@@ -36,6 +44,19 @@ export class ProjectComponent {
 
   payNow(project: Project) {
     this.commonService.navigate(`/payment/${project.id}`);
+  }
+
+
+  async loadImage() {
+    if (this.project.imageUrl !== '') {
+      try {
+        this.imageFile = await this.uploadService.getImage(this.project.imageUrl);
+        this.imageSrc = URL.createObjectURL(this.imageFile);
+        this.noImage = false
+      } catch (error) {
+        console.error('Failed to load image:', error);
+      }
+    }
   }
 }
 
