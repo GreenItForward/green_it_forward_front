041f74ce12082fdf62ce72c6da0457747951c6ea
James ABIB <james.bidule@gmail.com>
2023-07-12 16:36:50 +0200

    fix(home): Fix button text and add project navigation
    The commit fixes the button text in the home component's HTML file. It changes the text from "En savoir plus" to "Découvrir les projets". Additionally, it adds functionality to navigate to the projects page when the button is clicked. The changes are made in both the HTML and TypeScript files of the home component.
    
    This commit also includes minor code refactoring and cleanup in the TypeScript file, including adding a subscription for user login status and initializing projects and stats when a user is logged in.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { UserService } from 'src/app/services/user.service';
 import { UploadService } from 'src/app/services/upload.service';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Project } from 'src/app/models/project.model';
 import { CommonService } from 'src/app/services/common.service';
 import { StatsService } from 'src/app/services/stats.service';
 import { User } from 'src/app/models/user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +15,14 @@ import { User } from 'src/app/models/user.model';
 })
 export class HomeComponent implements OnInit {
   constructor(protected router: Router, private commonService: CommonService, private statsService: StatsService,
-    private uploadService: UploadService, protected userService: UserService) { }
+    private uploadService: UploadService, protected userService: UserService,
+    private changeDetector: ChangeDetectorRef) { }
 
   projects:Project[];
   nbProjects: number;
   nbUsers: number;  
   slides: { name: string; background: string; title: string; price: string; }[] = [];
+  private subscription: Subscription;
 
   async addSlides(projects: Project[]) {
     for (let project of projects) {
@@ -35,14 +38,10 @@ export class HomeComponent implements OnInit {
 
   slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
 
-  slickInit(e: any) {
-  }
-  breakpoint(e: any) {
-  }
-  afterChange(e: any) {
-  }
-  beforeChange(e: any) {
-  }  
+  slickInit(e: any) { }
+  breakpoint(e: any) { }
+  afterChange(e: any) { }
+  beforeChange(e: any) { }  
 
   async loadImage(imageUrl: string) {
       let imageFile;
@@ -57,18 +56,46 @@ export class HomeComponent implements OnInit {
 
 
   async ngOnInit(): Promise<void> {
+    this.subscription = this.userService.isLoggedIn$.subscribe((isLoggedIn: boolean) => {
+      if (isLoggedIn) {
+        this.initProjectsAndStats();
+      }
+      this.changeDetector.detectChanges();
+    });
+
+  }
+
+  
+
+  onDiscoverProjects() {
+    if (this.userService.isLoggedIn$) {
+      this.router.navigate(['/projects']);
+    }else {
+      this.commonService.navigate('/login');
+    }
+  }
+
+  async initProjectsAndStats() {
+    this.slides = [];
+
     this.commonService.getProjectIci().then(projects => {
       this.projects = projects;
       this.addSlides(this.projects);
     }).catch(error => {
       console.error(error);
     });
-
+  
     try {
       this.nbProjects = await this.statsService.getTotalProjects();
       this.nbUsers = await this.statsService.getTotalUsers();
     } catch (error) {
         console.error(error);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 } 
